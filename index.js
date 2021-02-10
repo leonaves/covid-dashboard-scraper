0f04cfde11dc9feb5284107d9a92f43eb39f0a5c
Leon Aves <leon@fiit.tv>
2021-02-10 17:21:19 +0000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,28 @@
+const captureWebsite = require('capture-website');
+const sendgrid = require('@sendgrid/mail');
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+const today = new Date();
+const formattedDate = today.toLocaleDateString('en-GB');
+
+(async () => {
+  const data = await captureWebsite.buffer('https://coronavirus.data.gov.uk/', { height: 1120 });
+
+  const email = {
+    to: process.env.RECIPIENT_EMAIL,
+    from: process.env.SENDER_EMAIL,
+    subject: `Covid dashboard update - ${formattedDate}`,
+    attachments: [
+      {
+        content: data.toString('base64'),
+        filename: 'screenshot.png',
+        type: 'image/png',
+        disposition: 'attachment',
+        contentId: '<screenshot@covid.dashboard>',
+      },
+    ],
+  };
+
+  await sendgrid.send(email);
+})();
